f43f00b4efc4beaacfef5c4071179e8fc5da0f40
Martin Andersen <martin@pinknose.dk>
2013-04-22 01:20:02 +0200

    - hgroup fjernet fra header fordi den er obsolete - siteundertitlen er gjort valgfri

--- wp-content/themes/pinknose/header.php
+++ wp-content/themes/pinknose/header.php
@@ -34,11 +34,19 @@
 <body <?php body_class(); ?>>
 <div id="page" class="hfeed site">
 	<header id="masthead" class="site-header" role="banner">
-		<hgroup>
-			<h1 class="site-title"><a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a></h1>
-			<h2 class="site-description"><?php bloginfo( 'description' ); ?></h2>
-		</hgroup>
-
+		<h1 class="site-title">
+			<a href="<?php echo esc_url( home_url( '/' ) ); ?>" title="<?php echo esc_attr( get_bloginfo( 'name', 'display' ) ); ?>" rel="home"><?php bloginfo( 'name' ); ?></a>
+			
+		</h1>
+		<?php
+			$blog_info = get_bloginfo( 'description' );
+			if ( $blog_info != '' ) {
+		?>
+		<p class="site-description"><?php echo $blog_info ?></p>
+		<?php
+			}
+		?>
+		
 		<nav id="site-navigation" class="main-navigation" role="navigation">
 			<h3 class="menu-toggle"><?php _e( 'Menu', 'twentytwelve' ); ?></h3>
 			<a class="assistive-text" href="#content" title="<?php esc_attr_e( 'Skip to content', 'twentytwelve' ); ?>"><?php _e( 'Skip to content', 'twentytwelve' ); ?></a>
